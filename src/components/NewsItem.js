60c3dd745db9bff5291b8448be1dcc0ac4ad3187
Abhishek Yadav <abhishekyadav9992183177@gmail.com>
2023-07-28 18:45:02 +0530

    fetched api and rendered in UI

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
     return (
     <div className="container">
       <div className="card" style={{width: "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://static.foxnews.com/foxnews.com/content/uploads/2023/06/Hunter-Biden-Laptop-Lawsuit-Deposition-1.jpg":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
           <h5 className="card-title">{title}</h5>
           <p className="card-text">{description }</p>
@@ -17,5 +17,5 @@ export class NewsItem extends Component {
     )
   }
 }
-
+ 
 export default NewsItem
